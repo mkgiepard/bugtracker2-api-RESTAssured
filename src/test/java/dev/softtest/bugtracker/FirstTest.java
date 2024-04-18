ee904346b43c1e164b45569028d1cfc03b4068fa
Mariusz <mkgiepard@gmail.com>
2024-04-18 15:19:39 +0200

    new> add support for dotenv-java to store test users credentials

--- src/test/java/dev/softtest/bugtracker/FirstTest.java
+++ src/test/java/dev/softtest/bugtracker/FirstTest.java
@@ -1,16 +1,26 @@
 package dev.softtest.bugtracker;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
-import org.junit.Test;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class FirstTest {
+    public static Dotenv env;
+
+    @BeforeClass
+    public static void setup() {
+        env = Dotenv.load();
+     }
 
     @Test
     public void testing_returns_200_with_expected_msg() {
+        System.out.println(env.get("USER1_USERNAME"));
+        System.out.println(env.get("USER1_PWD"));
 
         given().
             port(3000).
