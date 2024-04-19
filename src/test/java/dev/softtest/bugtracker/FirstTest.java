11c9fdae49d40f6fa62f6119abee13df85e3c89e
Mariusz <mkgiepard@gmail.com>
2024-04-19 14:03:52 +0200

    test> add first (post) request test for /auth/register

--- src/test/java/dev/softtest/bugtracker/FirstTest.java
+++ src/test/java/dev/softtest/bugtracker/FirstTest.java
@@ -3,11 +3,15 @@ package dev.softtest.bugtracker;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.*;
 
 public class FirstTest {
     public static Dotenv env;
@@ -30,5 +34,28 @@ public class FirstTest {
             statusCode(200).
             body("msg", equalTo("SUCCESS: not protected /testing route (3000)"));
 
+    }
+
+    @Test 
+    public void register_returns_200_and_confirmation_msg() {
+
+        Gson gson = new Gson();
+        Map<String, String> userMap = new LinkedHashMap<>();
+        userMap.put("username", "jhonny");
+        userMap.put("email", "jhonny@example.invalid");
+        userMap.put("password", "magic");
+        
+        // Serialization
+        String userJson = gson.toJson(userMap);
+
+        given().
+            port(3001).
+            contentType("application/json").
+            body(userJson.toString()).
+        when().
+            post("/auth/register").
+        then().
+            statusCode(200).
+            body("msg", equalTo("User '" + userMap.get("username") + "' successfully registered!"));
     }
 }
